4a98b78d869cd4e7d3dfca473e050e5b256acebe
jey <diolite.jn@gmail.com>
2020-05-04 19:23:21 +0900

    コンポーネントのPropsについて

--- src/App.js
+++ src/App.js
@@ -12,18 +12,28 @@ import React from 'react';
 //   }
 // }
 const App = () => {
+  const profiles = [
+    {name : "taro",age:10},
+    {name :"hanako",age:99},
+    {name:"jiro",age:100},
+    {name:"saburo"}
+  ]
   return (
     <React.Fragment>
-    <h1>Hi !</h1>
-    <Cat />
-    <Cat />
-    <Cat />
+      {
+        profiles.map((profile,index) => {
+         return <User name={profile.name} age={profile.age} key={index} />
+        })
+      }
     </React.Fragment>
   );
 }
 
-const Cat = () => {
-  return <div>Meo!</div>
+const User = (props) => {
+  return <div>I'm {props.name} and {props.age} years old.</div>
 }
 
+User.defaultProps = {
+  age : 1
+}
 export default App;
