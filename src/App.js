1f7b01bc14c890e7bc2cf66407ff3cb5ce884202
jey <diolite.jn@gmail.com>
2020-05-04 13:49:00 +0900

    コンポーネント

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component{
-  render() {
-    return (
-      <React.Fragment>
-        <label htmlFor="bar">bar</label>
-        <input type="text" onClick={()=>{console.log("i am click")}} />
-      </React.Fragment>
+// class App extends Component{
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         <input type="text" onClick={()=>{console.log("i am click")}} />
+//       </React.Fragment>
    
-    );
-  }
+//     );
+//   }
+// }
+const App = () => {
+  return (
+    <React.Fragment>
+    <h1>Hi !</h1>
+    <Cat />
+    <Cat />
+    <Cat />
+    </React.Fragment>
+  );
 }
+
+const Cat = () => {
+  return <div>Meo!</div>
+}
+
 export default App;
