93e8004981e51aed41f9c1534b3eddcce9f3e35b
jey <diolite.jn@gmail.com>
2020-05-04 21:46:11 +0900

    stateとsetStateの学習

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-import React from 'react';
-import propTypes from "prop-types";
-const App = () => {
-  const profiles = [
-    {name :"Taro",age:10},
-    {name :"Hanako",age:100},
-    {name:"Jiro",age:100},
-    {name:"Noname",age:999}
-  ]
-  return (
-    <React.Fragment>
-      {
-        profiles.map((profile,index) => {
-         return <User name={profile.name} age={profile.age} key={index} />
-        })
-      }
-    </React.Fragment>
-  );
+import React, { Component } from 'react';
+
+const App = () => { return <Counter />}
+
+class Counter extends Component {
+  constructor(props) {
+    super(props)
+    console.log(this.state)
+    this.state = {count :0}
+  }
+  handlePlusButton = () => {
+    this.setState({count : this.state.count + 1})
+  }
+  handleMinusButton = () => {
+    this.setState({count: this.state.count -1})
+  }
+  render () {
+    console.log(this.state)
+    return(
+      <React.Fragment>
+        <div>count:{this.state.count}</div>
+        <button onClick={this.handlePlusButton}>+1</button>
+        <button onClick={this.handleMinusButton}>-1</button>
+      </React.Fragment>
+    )
+   }
 }
 
-const User = (props) => {
-  return <div>I'm {props.name} and {props.age} years old.</div>
-}
-
-User.propTypes = {
-  name: propTypes.string,
-  age:propTypes.number.isRequired
-}
 export default App;
