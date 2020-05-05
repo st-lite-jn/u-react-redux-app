14ec8cd840787054a9a3e2b664de618d73cb382c
jey <diolite.jn@gmail.com>
2020-05-06 00:28:47 +0900

    Reduxで動作するようにした

--- src/reducers/count.js
+++ src/reducers/count.js
@@ -2,13 +2,13 @@ import {INCREMENT , DECREMENT} from "../actions"
 
 const initialState = {value:0}
 
-export default (state = initialState ,action)=> {
+export default (state = initialState , action)=> {
     switch(action.type) {
         case INCREMENT:
-            return {value:state.value +1 }
+            return {value: state.value +1 }
         case DECREMENT:
-            return {value:state - 1}
+            return {value: state.value -1 }
         default:
-            return state.value
+            return state
     }
 }
